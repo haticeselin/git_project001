d6567a21748d183a8313fe93e61c5734901cd532
Hatice Selin <haticeselinpelit@gmail.com>
2020-08-08 17:06:09 -0300

    the third commitfrom local master

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,10 +5,16 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Our first commit from local repo");
 		System.out.println("Our second commit from local repository");
+
 		System.out.println("Now i am on my hatice branch");
+
+
+		System.out.println("feature branch next commit");
+
+
 		System.out.println("I want to have conflict");
 		
 		System.out.println("I want to have a second conflict");
-	}
 
+	}
 }
